a4c513e16f3df862a1f17cf63dc08cd5d368b97d
Francis Kisiara <fkisiara@sendyit.com>
2020-07-20 16:59:04 +0300

    Finalising on the first version of SIWA

---
+++ src/tests/siwa.spec.js
@@ -0,0 +1,18 @@
+import { errors } from '../helpers'; 
+import Siwa from '../Siwa';
+
+const expect = require('chai').expect
+
+describe('Siwa', () => {
+  it ('should throw an error if there is no configuration passed', () => {
+    expect(() => new Siwa).to.throw(errors.NO_CONF);
+  });
+
+  it ('should throw an error if configuration is not an object', () => {
+    expect(() => new Siwa('conf')).to.throw(errors.INVALID_CONF);
+  });
+
+  it ('should throw an error if required fields dont exist', () => {
+    expect(() => new Siwa({})).to.throw(errors.MISSING_FIELDS);
+  });
+});
